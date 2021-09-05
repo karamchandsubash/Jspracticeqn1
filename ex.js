4c9cf6395f1ac00266eb2d60690aa2cc616e397a
KARAMCHAND SUBASH K <karamchandsubash@gmail.com>
2021-09-05 22:28:20 +0530

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,5 @@
+function Fun1() {
+    document.getElementById("demo").style.fontSize = "25px"; 
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.backgroundColor = "cyan";        
+  }
